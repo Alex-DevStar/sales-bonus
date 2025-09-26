8960a7aab299dde7983c1eb0df6e8972c993efa0
Aleksandr Chertkov <alexiiilaiho@gmail.com>
2025-09-26 15:18:02 +0300

    Stage 3 step 2

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@
 function calculateSimpleRevenue(purchase, _product) {
    // @TODO: Расчет выручки от операции
       const { discount, sale_price, quantity } = purchase;
-
+      return sale_price * quantity * (1 - discount / 100);
 }
 
 /**
@@ -36,12 +36,13 @@ function analyzeSalesData(data, options) {
      const { calculateRevenue, calculateBonus } = options; // Сюда передадим функции для расчётов
 
     // @TODO: Проверка входных данных
+
     if (
   data.purchase_records === undefined ||
   !Array.isArray(data.purchase_records) ||
   data.purchase_records.length === 0
 ) {
-  console.log("Некорректные данные о продажах");
+  throw new Error("Некорректные данные о продажах");
 }
 
  if (
@@ -49,7 +50,7 @@ function analyzeSalesData(data, options) {
   !Array.isArray(data.sellers) ||
   data.sellers.length === 0
 ) {
-  console.log("Некорректные данные о продавцах");
+  throw new Error("Некорректные данные о продавцах");
 }
 
  if (
@@ -57,27 +58,67 @@ function analyzeSalesData(data, options) {
   !Array.isArray(data.products) ||
   data.products.length === 0
 ) {
-  console.log("Некорректные данные о товарах");
+  throw new Error("Некорректные данные о товарах");
 }
 
     // @TODO: Проверка наличия опций
 
-
 if (options === undefined || options === null) {
-  console.log("В качестве функции ничего не передано")
+  throw new Error("В качестве функции ничего не передано")
 }
 
 if (typeof options.calculateRevenue !== 'function') {
-  console.log("Переданные значения в расчет доходов не являются функцией")
+  throw new Error("Переданные значения в расчет доходов не являются функцией")
 }
 
 if (typeof options.calculateBonus !== 'function') {
-  console.log("Переданные значения в расчет бонусов не являются функцией")
+  throw new Error("Переданные значения в расчет бонусов не являются функцией")
 }
 
     // @TODO: Подготовка промежуточных данных для сбора статистики
-
+  const sellerStats = data.sellers.map((seller) => ({
+  id: seller.id,
+  name: `${seller.first_name} ${seller.last_name}`,
+  revenue: 0,
+  profit: 0,
+  sales_count: 0,
+  products_sold: {}
+}));
     // @TODO: Индексация продавцов и товаров для быстрого доступа
+
+  const sellerIndex = sellerStats.reduce((acc, seller) => {
+  acc[seller.id] = seller;
+  return acc;
+}, {});
+
+
+const productIndex = data.products.reduce((acc, product) => {
+acc[product.sku] = product;
+return acc;
+}, {});
+
+
+data.purchase_records.forEach(record => { // Чек
+        const seller = sellerIndex[record.seller_id]; // Продавец
+          seller.sales_count++;            // Увеличить количество продаж
+          seller.revenue+= record.total_amount;          // Увеличить общую сумму всех продаж
+
+        // Расчёт прибыли для каждого товара
+        record.items.forEach(item => {
+            const product = productIndex[item.sku]; // Товар
+           const cost = item.purchase_price * item.quanity;// Посчитать себестоимость (cost) товара как product.purchase_price, умноженную на количество товаров из чека
+            const revenue = calculateSimpleRevenue(item, product)// Посчитать выручку (revenue) с учётом скидки через функцию calculateRevenue
+            // Посчитать прибыль: выручка минус себестоимость
+        // Увеличить общую накопленную прибыль (profit) у продавца
+
+            // Учёт количества проданных товаров
+            if (!seller.products_sold[item.sku]) {
+                seller.products_sold[item.sku] = 0;
+            }
+            seller.products_sold[item.sku] += item.quanity;
+            // По артикулу товара увеличить его проданное количество у продавца
+        });
+ });
 
     // @TODO: Расчет выручки и прибыли для каждого продавца
 
@@ -86,15 +127,4 @@ if (typeof options.calculateBonus !== 'function') {
     // @TODO: Назначение премий на основе ранжирования
 
     // @TODO: Подготовка итоговой коллекции с нужными полями
-    const sellersMap = new Map();
-data.sellers.forEach((seller) => {
-  sellersMap.set(seller.id,
-    {
-    id: seller.id,
-    name: `${seller.first_name} ${seller.last_name}`,
-    revenue: 0,
-    profit: 0,
-    sales_count: 0,
-    products_sold: {}
-  });
-});
+
